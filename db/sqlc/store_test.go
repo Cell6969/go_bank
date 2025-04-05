9a05a5bbd3567fd8b0e665f0689a46fcf53f3945
Cell6969 <bossmarinoo@gmail.com>
2025-04-05 13:54:50 +0700

    add: deadlock scenario with resolving

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -11,6 +11,8 @@ import (
 func TestTransferTx(t *testing.T) {
 
 	store := NewStore(testDb)
+	testQueries.ResetTransferTable(context.Background())
+	testQueries.ResetEntryTable(context.Background())
 	testQueries.ResetAccountTable(context.Background())
 
 	account1 := createRandomAccount(t)
@@ -91,7 +93,7 @@ func TestTransferTx(t *testing.T) {
 		require.Equal(t, account2.ID, toAccount.ID)
 
 		// TODO: check account balance
-		fmt.Println(">>> tx:", account1.Balance, account2.Balance)
+		fmt.Println(">>> tx:", fromAccount.Balance, toAccount.Balance)
 		diff1 := account1.Balance - fromAccount.Balance
 		diff2 := toAccount.Balance - account2.Balance
 		require.Equal(t, diff1, diff2)
@@ -114,5 +116,65 @@ func TestTransferTx(t *testing.T) {
 	require.Equal(t, account1.Balance-int64(n)*amount, updateAccount1.Balance)
 	require.Equal(t, account2.Balance+int64(n)*amount, updateAccount2.Balance)
 
-	fmt.Println(">>> after:", account1.Balance, account2.Balance)
+	fmt.Println(">>> after:", updateAccount1.Balance, updateAccount2.Balance)
+}
+
+/*
+*
+This scenario for deadlock operation where account1 tx to account2 at same time account2 tx to account1
+*/
+func TestTransferTxDeadlock(t *testing.T) {
+
+	store := NewStore(testDb)
+	testQueries.ResetAccountTable(context.Background())
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	fmt.Println(">>> before:", account1.Balance, account2.Balance)
+
+	// run n concurrency
+	n := 10
+	amount := int64(10)
+
+	errs := make(chan error)
+
+	for i := 0; i < n; i++ {
+		fromAccountId := account1.ID
+		toAccountId := account2.ID
+
+		// make condition for account1 transfer to account2 and vice versa
+		if i%2 == 0 {
+			fromAccountId = account2.ID
+			toAccountId = account1.ID
+		}
+		go func() {
+			ctx := context.Background()
+			arg := TransferTxParams{
+				FromAccountId: fromAccountId,
+				ToAccountId:   toAccountId,
+				Amount:        amount,
+			}
+			_, err := store.TransferTx(ctx, arg)
+
+			errs <- err
+		}()
+	}
+
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+
+	}
+
+	// check the final updated balances
+	updateAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updateAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	require.Equal(t, account1.Balance, updateAccount1.Balance)
+	require.Equal(t, account2.Balance, updateAccount2.Balance)
+
+	fmt.Println(">>> after:", updateAccount1.Balance, updateAccount2.Balance)
 }
