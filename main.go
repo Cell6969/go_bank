de8c2411de79ed35a5b24ebf3627138c0a7701c5
Cell6969 <bossmarinoo@gmail.com>
2025-04-06 11:15:01 +0700

    update: use load env

--- main.go
+++ main.go
@@ -6,18 +6,18 @@ import (
 
 	"github.com/Cell6969/go_bank/api"
 	db "github.com/Cell6969/go_bank/db/sqlc"
+	"github.com/Cell6969/go_bank/util"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgres://root:root@localhost:5432/simple_bank?sslmode=disable"
-	address  = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
 	}
@@ -25,7 +25,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(address)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("Cannot run server HTTP:", err)
 	}
